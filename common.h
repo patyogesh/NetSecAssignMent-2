133988fcd06812ee41b11db877501ac06312cc17
patyogesh <patyogesh@gmail.com>
2014-09-08 21:31:28 -0400

    added some enums, client code, error handling

--- common.h
+++ common.h
@@ -4,5 +4,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>                                                                                                                                                 
+#include <sys/types.h>                                                                                                                                                  
+#include <netinet/in.h>                                                                                                                                                 
+#include <netdb.h> 
+#include <arpa/inet.h>
+
+struct sockaddr_in server_addr;
+
+#define BUFFER_SIZE 1024
+#define SERVER_PORT 2346
+
+int cryp_sock_fd = 0;
+int dec_sock_fd = 0;
+
+char sendBuffer[BUFFER_SIZE];
+char recvBuffer[BUFFER_SIZE];
+
+typedef enum errors {
+    SUCCESS,
+    FAILURE,
+    CLIENT_FAIL,
+    SERVER_FAIL,
+    SOCKET_FAIL,
+    CONNECT_FAIL,
+    BIND_FAIL,
+    RECV_FAIL,
+    SEND_FAIL,
+    FREAD_FAIL,
+    FWRITE_FAIL,
+    FOPEN_FAIL,
+    UNEXPECTED
+} 
+Error_t;
+
+
 
 #endif
--- crypt.c
+++ crypt.c
@@ -1,6 +1,95 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include "common.h"
+
+#define HOW_TO_USE {\
+    printf("gatorcrypt <input file> [-d < IP-addr:port >][-l] \n\n"); \
+    printf("Description: \n\n");\
+    printf(" -d < IP-Address:port > :\n");\
+    printf("     Destination machines IP and port address pair to dump file to \n");\
+    printf(" -l :\n");\
+    printf("     Local Mode: dump file to local machines\n");\
+}
+
+int init_client(int server_port, char *server_ip, FILE *fptr)
+{
+    cryp_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(cryp_sock_fd < 0) {
+
+	printf("\n Error opening socket");
+	return SOCKET_FAIL;
+    }
+
+    memset(&server_addr, '0', sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+
+    if(inet_pton(AF_INET, server_ip, &server_addr.sin_addr) < 0) {
+
+	printf("inet_pton Error\n");
+	return CLIENT_FAIL;
+    }
+
+    if(connect(cryp_sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+
+	printf("Error while connecting to server\n");
+	return CONNECT_FAIL;
+    }
+
+    return SUCCESS;
+}
+
+
+int extract_server_ip_port(char *args, char **server_ip, int *server_port)
+{
+    int i = 0, j = 0;
+
+    *server_ip = args;
+    while(args[i] != ':') {
+	i++;
+    }
+    args[i] = '\0';
+    i++;
+
+    char port[5];
+    while(args[i]) {
+	port[j] = args[i];
+	j++;
+	i++;
+    }
+    port[j] = '\0';
+
+    *server_port = atoi(port);
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
+    char *server_ip = NULL;
+    int server_port = 0;
+
+    if(argc < 2) {
+	printf("ERROR: Insufficient Arguments\n");
+	HOW_TO_USE;
+	return FAILURE;
+    }
+
+    FILE *fptr = fopen(argv[1], "r+");
+    
+    if(NULL == fptr) {
+
+	printf("ERROR: Error while opening input file %s, please check if file exists\n", argv[1]); 
+	return FOPEN_FAIL;
+    }
+
+    extract_server_ip_port(argv[2], &server_ip, &server_port);
+
+    init_client(server_port, server_ip, fptr);
+
+
     return 0;
 }
